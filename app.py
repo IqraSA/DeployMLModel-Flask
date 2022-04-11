ebd1a0b537683eb3bf7b39b3ad74234504744536
Sourcery AI <>
2022-04-11 09:29:08 +0000

    'Refactored by Sourcery'

--- app.py
+++ app.py
@@ -20,7 +20,9 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template(
+        'index.html', prediction_text=f'Employee Salary should be $ {output}'
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
